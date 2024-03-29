668fdf2675e009be851178b4fe2667e04c025c9c
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2024-03-29 17:12:39 +0100

    use plotly

--- src/ChartArea.tsx
+++ src/ChartArea.tsx
@@ -1,11 +1,23 @@
+import Plot from 'react-plotly.js';
 
 export default function ChartArea() {
   
-  return (
-    <div className="w-screen h-screen pt-7">
-        <div className="w-full h-full">
-            <div></div>
+    return (
+        <div className="w-screen h-screen pt-7">
+            <div className="w-full h-full">
+            <Plot
+                data={[
+                {
+                    x: [1, 2, 3],
+                    y: [2, 6, 3],
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    marker: {color: 'red'},
+                },
+                ]}
+                layout={{width: 1000, height: 500, title: 'A Fancy Plot'}}
+            />
+            </div>
         </div>
-    </div>
-  );
+    );
 }
