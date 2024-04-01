77ffe462b51141bb14cc8a7d0ba676e8578bdcb4
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2024-04-01 18:24:33 +0200

    light theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import Dashboard from './Dashboard';
 import { PrimeReactProvider } from 'primereact/api';
 
-import 'primereact/resources/themes/lara-dark-indigo/theme.css'; //theme
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
 import 'primereact/resources/primereact.min.css'; //core css
 import 'primeicons/primeicons.css'; //icons
 import 'primeflex/primeflex.css'; // flex
