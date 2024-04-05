98a5dd6a760e6ac9bdbe68560f2bd228814ba10b
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2024-04-05 17:46:48 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Configure the dev server
+  server: {
+    host: '0.0.0.0', // Expose the server to other devices on the network
+    port: 5173,
+    open: true, // Open the browser automatically when server starts
+  },
 })
